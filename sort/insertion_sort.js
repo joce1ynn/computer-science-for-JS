5de546bddcec1e62978fcefc3914eb9733023996
Wenwen Tian <wwtian9@gmail.com>
2022-08-16 17:15:11 -0600

    fix insertion sort

--- sort/insertion_sort.js
+++ sort/insertion_sort.js
@@ -4,8 +4,8 @@ function insertionSort(arr) {
     currentVal = arr[i];
     for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j];
-      arr[j] = currentVal;
     }
+    arr[j + 1] = currentVal;
   }
   return arr;
 }
