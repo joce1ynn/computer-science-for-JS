54f0367ef97966c22a4ec630fa4e1121fca4982d
Wenwen Tian <wwtian9@gmail.com>
2022-08-23 09:04:09 -0600

    add static method

--- data structures/class_instance.js
+++ data structures/class_instance.js
@@ -24,7 +24,29 @@ class Student {
     let sum = this.scores.reduce((a, b) => a + b);
     return sum / this.scores.length;
   }
+  static enrollStudents() {
+    return "ENROLLING STUDENTS!";
+  }
 }
 
 let firstStudent = new Student("Colt", "Steele", 1);
 let secondStudent = new Student("Blue", "Steele", 2);
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static distance(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+
+    return Math.hypot(dx, dy);
+  }
+}
+
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
+
+console.log(Point.distance(p1, p2)); // 7.0710678118654755
